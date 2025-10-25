c9cea7fe12491740ac8d7443098e546c91ae3351
Kartik Shenoy <kartik.shenoyy@gmail.com>
2025-10-24 17:21:10 -0700

    Remove static exports

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   // Export the app as static HTML (next export)
-  output: "export",
-  // basePath: isProd ? '/hpv-chat-assistant' : '',
-  // assetPrefix: isProd ? '/hpv-chat-assistant/' : '',
-  images: {
-    unoptimized: true, // GitHub Pages does not support Next.js image optimization by default
-  },
+  // output: "export",
+  // // basePath: isProd ? '/hpv-chat-assistant' : '',
+  // // assetPrefix: isProd ? '/hpv-chat-assistant/' : '',
+  // images: {
+  //   unoptimized: true, // GitHub Pages does not support Next.js image optimization by default
+  // },
 };
 
 export default nextConfig;
