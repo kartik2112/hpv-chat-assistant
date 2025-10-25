31b98cc19616a294cd69c238a522b18d8a8d9fba
Kartik Shenoy <kartik.shenoyy@gmail.com>
2025-10-24 17:17:47 -0700

    Route changes for Vercel

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   // Export the app as static HTML (next export)
   output: "export",
-  basePath: isProd ? '/hpv-chat-assistant' : '',
-  assetPrefix: isProd ? '/hpv-chat-assistant/' : '',
+  // basePath: isProd ? '/hpv-chat-assistant' : '',
+  // assetPrefix: isProd ? '/hpv-chat-assistant/' : '',
   images: {
     unoptimized: true, // GitHub Pages does not support Next.js image optimization by default
   },
